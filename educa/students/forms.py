7dfc47e5e2ebe9b276a3129a40a189f40722c403
yanka125 <yanka7356@gmail.com>
2022-02-06 19:24:02 +0300

    Online school app

---
+++ educa/students/forms.py
@@ -0,0 +1,6 @@
+from courses.models import Course
+from django import forms
+
+
+class CourseEnrollForm(forms.Form):
+	course = forms.ModelChoiceField(queryset=Course.objects.all(), widget=forms.HiddenInput)
